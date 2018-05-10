cd60a8847623fa305711b2c77f43861bcd9c7232
ela-p-p <elizabeth.porter.pevec@gmail.com>
2018-05-09 22:48:08 -0400

    hide/show on start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,20 @@
 $(document).ready(function( $ ) {
 
-    var gameStart = randomNum()
-    function randomNum(){
-        var random = Math.floor(Math.random() * 102) + 19;
-        $("#randomNumber").text(random); 
-        return random
-    }
+    $(".card-subtitle").hide();//hide time remaining and question before Start pressed
+    $("#buttons").hide();
+    $("#startBtn").click(function() { //start pressed, start button hides, time remaining and question display
+    $(this).hide('fast', function(){
+           $(".card-subtitle").show('fast', function(){
+               $("#buttons").show('fast', function(){
+
+               });//this hide
+           });//coutdonw show
+       });//question show
+
+     });//start button click
+     
+    
+    
 
 
 }); //end doc ready
